34ff2d3ed2f007a8efd8bf64e3b3edc34be77be4
Miguel Ángel Latre Abadía <latre@unizar.es>
2023-10-04 16:20:39 +0200

    Función main para el problema 3 curso 2023-24

--- 3-repituno-con-funciones.cpp
+++ 3-repituno-con-funciones.cpp
@@ -14,7 +14,7 @@ using namespace std;
  *       En caso contrario, ha devuelto -1.
  */
 int ordenRepituno(unsigned n) {
-    bool esRepituno = true;
+    bool esRepituno = n > 0;
     unsigned orden = 0;
     while (n != 0 && esRepituno) {
         esRepituno = (n % 10 == 1);
@@ -31,15 +31,22 @@ int ordenRepituno(unsigned n) {
 
 
 /*
- * Programa que, a modo de prueba de la función «repituno», ha escrito en la 
- * pantalla los valores devueltos por algunas invocaciones a la misma
+ * Programa que solicita al usuario un número entero y, si es un número repituno, escribe en la
+ * pantalla el su orden. En caso contrario, indica que el número introducido no es repituno.
  */
 int main() {
-    cout << ordenRepituno(0) << " = 0" << endl;
-    cout << ordenRepituno(1) << " = 1" << endl;
-    cout << ordenRepituno(11) << " = 2" << endl;
-    cout << ordenRepituno(111111) << " = 6" << endl;
-    cout << ordenRepituno(110111) << " = -1" << endl;
-    cout << ordenRepituno(211111) << " = -1" << endl;
-    cout << ordenRepituno(111113) << " = -1" << endl;
+    // Petición y lectura del número
+    cout << "Escriba un número entero: ";
+    int numero;
+    cin >> numero;
+
+    // Cálculo de si es repituno y su orden
+    int orden = ordenRepituno(numero);
+
+    // Discriminación y escritura del resultado
+    if (orden >= 0) {
+        cout << numero << " es repituno y su orden es " << orden << "." << endl;
+    } else {
+        cout << numero << " no es repituno." << endl;
+    }
 }
