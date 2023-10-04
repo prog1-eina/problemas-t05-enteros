282d43614837637631353ed74e3313bb1597aa6e
Miguel Ángel Latre Abadía <latre@unizar.es>
2023-10-04 13:57:07 +0200

    Añadidas versiones con funciones

---
+++ 3-repituno-con-funciones.cpp
@@ -0,0 +1,45 @@
+﻿/*******************************************************************************
+ * Autores: Javier Martínez y Miguel Ángel Latre
+ * Última revisión: 15-10-2021
+ * Resumen: Función que calcula el orden de un número repituno y programa de
+ *          prueba de la misma.
+ *          Problemas del tema 5 (enteros). Programación 1
+ ******************************************************************************/
+#include <iostream>
+using namespace std;
+
+/*
+ * Pre:  ---
+ * Post: Si «n» es un número repituno, ha devuelto el orden del mismo.
+ *       En caso contrario, ha devuelto -1.
+ */
+int ordenRepituno(unsigned n) {
+    bool esRepituno = true;
+    unsigned orden = 0;
+    while (n != 0 && esRepituno) {
+        esRepituno = (n % 10 == 1);
+        orden++;
+        n = n / 10;
+    }
+
+    if (esRepituno) {
+        return orden;
+    } else {
+        return -1;
+    }
+}
+
+
+/*
+ * Programa que, a modo de prueba de la función «repituno», ha escrito en la 
+ * pantalla los valores devueltos por algunas invocaciones a la misma
+ */
+int main() {
+    cout << ordenRepituno(0) << " = 0" << endl;
+    cout << ordenRepituno(1) << " = 1" << endl;
+    cout << ordenRepituno(11) << " = 2" << endl;
+    cout << ordenRepituno(111111) << " = 6" << endl;
+    cout << ordenRepituno(110111) << " = -1" << endl;
+    cout << ordenRepituno(211111) << " = -1" << endl;
+    cout << ordenRepituno(111113) << " = -1" << endl;
+}
---
+++ 4-quitar-cifra-con-funciones.cpp
@@ -0,0 +1,56 @@
+﻿/*********************************************************************************************\
+ * Autores: Javier Martínez y Miguel Ángel Latre
+ * Última revisión: 4-10-2023
+ * Resumen: Programa que solicita al usuario un número natural y escribe en la pantalla los
+ *          números resultantes de suprimir todas las ocurrencias de cada una de las cifras del
+ *          0 al 9 en el número escrito por el usuario.
+ *          Problemas del tema 5 (enteros). Programación 1
+\*********************************************************************************************/
+#include <iostream>
+using namespace std;
+
+/*
+ * Pre:  0 ≤ c ≤ 9
+ * Post: Ha devuelto un entero que, escrito en base 10, equivale
+ *       al resultado de suprimir todas las ocurrencias de la
+ *       cifra «c» en el entero «n» cuando se escribe en base 10.
+ *       Ejemplos:
+ *           quitarCifra(902037122, 0) = 9237122
+ *           quitarCifra(902037122, 1) = 90203722
+ *           quitarCifra(902037122, 2) = 900371
+ *           quitarCifra(902037122, 4) = 902037122
+ *           quitarCifra(902037122, 9) = 2037122
+ */
+unsigned quitarCifra(unsigned n, unsigned c) {
+    unsigned resultado = 0;
+    unsigned potencia10 = 1;
+    while (n != 0) {
+        if (n % 10 != c) {
+            resultado = resultado + (n % 10) * potencia10;
+            potencia10 = 10 * potencia10;
+        }
+        n = n / 10;
+    }
+    return resultado;
+}
+
+
+/*
+ * Programa que solicita al usuario un número natural y escribe en la pantalla los números
+ * resultantes de suprimir todas las ocurrencias de cada una de las cifras del 0 al 9 en el
+ * número escrito por el usuario.
+ */
+int main() {
+    // Petición del dato y filtrado de la entrada de usuario:
+    int numero;
+    do {
+        cout << "Escriba un número natural: ";
+        cin >> numero;
+    } while (numero <= 0);
+
+    // Para cada cifra entre 0 y 9
+    for (unsigned cifra = 0; cifra <= 9; cifra++) {
+        cout << "El número " << numero << " sin el " << cifra << " es " 
+             << quitarCifra(numero, cifra) << endl;
+    }
+}
